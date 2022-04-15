1d394c07823979cbfed5c4d4740a1fd3793b5133
AndhikaK <andhika.smandading@gmail.com>
2022-04-15 10:54:19 +0700

    finished the new pasal mechanism

--- resources/views/pages/archive-create.blade.php
+++ resources/views/pages/archive-create.blade.php
@@ -1,56 +1,70 @@
 @extends('layouts.app-layout')
 
 @section('content')
-    <div class="p-3 bg-white rounded-md border border-slate-400 border-t-2 border-t-rose-700">
-        <div class="flex justify-end w-full">
+    {{-- <div class="p-3 bg-white rounded-md border border-slate-400 border-t-2 border-t-rose-700"> --}}
+    {{-- <div class="flex justify-end w-full">
             <a href="{{ route('archive-file.create') }}">
                 <button class="bg-sky-800 p-2 text-white font-medium rounded">
                     File upload
                 </button>
             </a>
         </div>
-    </div>
+    </div> --}}
 
     <div id='container'>
-        <ul id='sortable-input'>
-            <li class="my-2 rounded-xl shadow-lg border border-slate-300 border-l-4 border-l-sky-600">
-                <div class="header py-2 text-center flex justify-center border-b border-b-slate-400/20 cursor-move">
+        <ul id='sortable-input' class="grid gap-2">
+            <li class="border border-slate-300 rounded-lg overflow-hidden shadow-lg bg-white">
+                <div class="header py-2 flex justify-center hover:bg-slate-100 border-b border-b-slate-300 cursor-move">
                     <img src="{{ asset('assets/svg/grabber.svg') }}" class="rotate-90">
                 </div>
-                <div class="title p-3 pb-0 font-semibold text-lg">
-                    Pasal
+                <div class="title px-3 py-2 pb-3 font-bold">Pasal</div>
+                <div class="content">
+                    <div class="content-item px-3 grid grid-cols-[1fr_auto_auto]">
+                        <textarea name="" class="ayat-input w-full h-5 border-0 border-b-2 overflow-y-auto focus:ring-0 p-2 pt-0"></textarea>
+                        <div class="content-grab mb-5 cursor-move flex items-center hover:bg-slate-200 h-full">
+                            <img src="{{ asset('assets/svg/grabber.svg') }}" class="p-2">
+                        </div>
+                        <div class="content-grab mb-5 cursor-pointer flex items-center h-full">
+                            <img src="{{ asset('assets/svg/x.svg') }}" onclick="removeInput(this)" class="p-2 rounded-full hover:bg-slate-200">
+                        </div>
+                    </div>
                 </div>
-                <div class="content py-3">
-                    <div class="content-item group grid grid-cols-[auto_1fr] w-full">
-                        <div class="content-grab px-1 cursor-move flex items-center">
-                            <img src="{{ asset('assets/svg/grabber.svg') }}" class="invisible group-hover:visible">
+                <div class="flex justify-end gap-1 border-t border-t-slate-200 mt-2 p-2">
+                    <button type="button" onclick="addInput(this)">
+                        <img src="{{ asset('assets/svg/plus-circle.svg') }}" class="p-2 rounded-full fill-slate-600 hover:fill-slate-900 hover:bg-slate-200">
+                    </button>
+                    <button type="button" onclick="removePasal(this)">
+                        <img src="{{ asset('assets/svg/trash.svg') }}" class="p-2 rounded-full fill-slate-600 hover:fill-slate-900 hover:bg-slate-200">
+                    </button>
+                </div>
+            </li>
+            <li class="border border-slate-300 rounded-lg overflow-hidden shadow-lg bg-white">
+                <div class="header py-2 flex justify-center hover:bg-slate-100 border-b border-b-slate-300 cursor-move">
+                    <img src="{{ asset('assets/svg/grabber.svg') }}" class="rotate-90">
+                </div>
+                <div class="title px-3 py-2 pb-3 font-bold">Pasal</div>
+                <div class="content">
+                    <div class="content-item px-3 grid grid-cols-[1fr_auto_auto]">
+                        <textarea name="" class="ayat-input w-full h-5 border-0 border-b-2 overflow-y-auto focus:ring-0 p-2 pt-0"></textarea>
+                        <div class="content-grab mb-5 cursor-move flex items-center hover:bg-slate-200 h-full">
+                            <img src="{{ asset('assets/svg/grabber.svg') }}" class="p-2">
                         </div>
-                        <div class="pr-5">
-                            <div class="ayat-title mb-2">Ayat</div>
-                            <textarea name="" class="ayat-input w-full p-1" onkeyup="$(this).height(0);$(this).height($(this).prop('scrollHeight'))"></textarea>
-                        </div>
-                    </div>
-                    <div class="content-item group grid grid-cols-[auto_1fr] w-full">
-                        <div class="content-grab px-1 cursor-move flex items-center">
-                            <img src="{{ asset('assets/svg/grabber.svg') }}" class="invisible group-hover:visible">
-                        </div>
-                        <div class="pr-5">
-                            <div class="ayat-title mb-2">Ayat</div>
-                            <textarea name="" class="ayat-input w-full p-1" onkeyup="$(this).height(0);$(this).height($(this).prop('scrollHeight'))"></textarea>
-                        </div>
-                    </div>
-                    <div class="content-item group grid grid-cols-[auto_1fr] w-full">
-                        <div class="content-grab px-1 cursor-move flex items-center">
-                            <img src="{{ asset('assets/svg/grabber.svg') }}" class="invisible group-hover:visible">
-                        </div>
-                        <div class="pr-5">
-                            <div class="ayat-title mb-2">Ayat</div>
-                            <textarea name="" class="ayat-input w-full p-1" onkeyup="$(this).height(0);$(this).height($(this).prop('scrollHeight'))"></textarea>
+                        <div class="content-grab mb-5 cursor-pointer flex items-center h-full">
+                            <img src="{{ asset('assets/svg/x.svg') }}" onclick="removeInput(this)" class="p-2 rounded-full hover:bg-slate-200">
                         </div>
                     </div>
+                </div>
+                <div class="flex justify-end gap-1 border-t border-t-slate-200 mt-2 p-2">
+                    <button type="button" onclick="addInput(this)">
+                        <img src="{{ asset('assets/svg/plus-circle.svg') }}" class="p-2 rounded-full fill-slate-600 hover:fill-slate-900 hover:bg-slate-200">
+                    </button>
+                    <button type="button" onclick="removePasal(this)">
+                        <img src="{{ asset('assets/svg/trash.svg') }}" class="p-2 rounded-full fill-slate-600 hover:fill-slate-900 hover:bg-slate-200">
+                    </button>
                 </div>
             </li>
         </ul>
+        <button type="button" onclick="addPasal(this)" class="py-1 px-2 rounded-sm bg-sky-500 mt-3 text-white font-semibold">Pasal baru</button>
     </div>
 @endsection
 
